78ab297bc343f3af046db8fc14e1b4d0e08cbe28
Fabio Bratta <fabio.bratta@dshare.com>
2022-02-04 10:46:29 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,6 +63,7 @@ export default function Home({ posts, pagination }) {
 export async function getInitialProps() {
   const { posts, pagination } = await getPaginatedPosts();
   console.log('ciao bellA', !!posts);
+  console.log('ciao bellO', Array.isArray(posts));
   return {
     props: {
       posts,
