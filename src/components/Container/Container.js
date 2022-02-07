b7bf68786348f16b13b78f303e176c87687bbbad
Fabio Bratta <fabio.bratta@dshare.com>
2022-02-07 19:29:30 +0100

    WIP

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,12 +2,16 @@ import ClassName from 'models/classname';
 
 import styles from './Container.module.scss';
 
-const Container = ({ children, className }) => {
+const Container = ({ children, className, sharingBtn }) => {
   const containerClassName = new ClassName(styles.container);
 
   containerClassName.addIf(className, className);
 
-  return <div className={containerClassName.toString()}>{children}</div>;
+  return (
+    <div className={containerClassName.toString()} data-sharing={sharingBtn}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
